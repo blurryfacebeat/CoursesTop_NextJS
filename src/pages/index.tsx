2561d406245e6e031a0c332d39e585697599c035
blurryfacebeat <blurryfacebeat@gmail.com>
2022-01-03 22:40:31 +0700

    Add paragraph component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,7 @@
 import React from 'react';
-import {
-  Button,
-  ButtonPrimary,
-  ButtonSecondary,
-  HeadingTag,
-} from '@/components';
 
 const HomePage = () => {
-  return (
-    <div>
-      <HeadingTag Tag="h1">Привет Мир!!!</HeadingTag>
-      <Button text="Hello World!!!" />
-      <ButtonPrimary text="Hello World2!!!" />
-      <ButtonSecondary text="Hello World3!!!" />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default HomePage;
